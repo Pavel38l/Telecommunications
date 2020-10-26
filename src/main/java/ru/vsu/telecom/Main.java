bfc8ba1518a0eb2c5dc59dd1c6c4142b5f94a606
Burdyug Pavel <serhioburdug@gmail.com>
2020-10-26 16:42:47 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/telecom/Main.java
@@ -0,0 +1,10 @@
+package ru.vsu.telecom;
+
+/**
+ * @author Pavel_Burdyug
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
